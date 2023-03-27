575f2a5da526eb151476e22509640ca55c5027e9
Nikoolaii <nikolailemerre@gmail.com>
2023-03-27 15:04:41 +0200

    Edit homepage (responsive + add animations)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        text: "text 5s ease infinite, fade 1s",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+        fade: {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-animation-delay")],
+};
